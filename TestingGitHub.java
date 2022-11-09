365f3908331b347290e6d9edec97c8b615809d5c
Jonathan-Pritchard <jpritchard1388@students.mc3.edu>
2022-11-09 17:43:34 -0500

    Test program
    This is for Week 11 Assignment

---
+++ TestingGitHub.java
@@ -0,0 +1,15 @@
+
+public class TestingGitHub {
+
+   public static void main(String[] args) {
+   
+      System.out.println("Hello World");
+      
+      for (int i = 0; i < 100; i++) {
+      
+         System.out.print(i+1 + ", ");
+      }
+   
+   
+   }  
+}
